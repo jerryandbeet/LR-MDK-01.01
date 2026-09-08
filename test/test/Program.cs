78b4923251f20df665d470bebd9d9901c96bc630
Anon1m4k <kirilvalera2@gmail.com>
2026-09-08 10:23:05 +0300

    Реализовал ввод значений в сравниваемые переменные с клавиатуры

--- test/test/Program.cs
+++ test/test/Program.cs
@@ -10,7 +10,13 @@ namespace test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!!!");
+            Console.WriteLine("Введите 1 число: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите 2 число: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите 3 число: ");
+            int c = Convert.ToInt32(Console.ReadLine());
+           
         }
     }
 }
