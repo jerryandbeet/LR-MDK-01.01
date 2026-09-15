6cd9c893c7bb98b29d9ecba08b90b5c2b0ce3078
qwizza <meowmeowpiupiu@mail.ru>
2026-09-15 09:18:13 +0300

    Создал функцию для чтения и валидации числового ввода в консоль

--- LAB1/LAB1/PizzaСalculation.cs
+++ LAB1/LAB1/PizzaСalculation.cs
@@ -8,6 +8,29 @@ namespace LAB1
 {
     class PizzaСalculation
     {
-        
-    }
+        public static int ReadInt(string prompt, bool IsPizzaSize)
+        {
+            Console.Write(prompt);
+            if (IsPizzaSize)
+            {
+                int count = Convert.ToInt32(Console.ReadLine());
+                while (count != 6 || count != 8 || count != 10)
+                {
+                    Console.Write("\nВы ввели неверное количество кусочков! Попробуйте еще раз (6, 8 или 10 кусочков): ");
+                    count = Convert.ToInt32(Console.ReadLine());
+                }
+                return count;
+            }
+            else
+            {
+                int count = Convert.ToInt32(Console.ReadLine());
+                while (count <= 0)
+                {
+                    Console.Write("\nВы ввели неверное количество! Попробуйте еще раз: ");
+                    count = Convert.ToInt32(Console.ReadLine());
+                }
+                return count;
+            }
+        }
+        }
 }
