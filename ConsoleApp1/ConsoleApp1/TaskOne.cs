e5a6becf3f9505f7b0210dc95cf587f8d5728401
qwizza <meowmeowpiupiu@mail.ru>
2026-09-19 11:50:59 +0300

    Решил первый вариант

---
+++ ConsoleApp1/ConsoleApp1/TaskOne.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class TaskOne
+    {
+        private static int[] readArray()
+        {
+            Console.WriteLine("Введите количество элементов массива:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[n];
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"\nВведите значение в ячейку массива [{i}] : ");
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            return array;
+        }
+
+        private static int sumArrays(int[] array)
+        {
+            int sum = 0;
+            foreach(int num in array)
+            {
+                if (num > 0)
+                    sum+=num;
+
+            }
+            return sum;
+        }
+
+        public static void run ()
+        {
+            Console.WriteLine("Сумма положительных элементов массива = " + sumArrays(readArray()));
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/TaskTwo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class TaskTwo
+    {
+        private static int[] newArray()
+        {
+            Random random = new Random();
+            int n = random.Next(1,101);
+            int[] array = new int[n];
+            Console.WriteLine($"Количество элементов массива: {array.Length}");
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(-500, 501);
+            }
+            return array;
+        }
+
+        private static List <int> newNumbers(int[] array)
+        {
+            List<int> list = new List<int>();
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 7 == 0) list.Add(array[i]);              
+            }
+            return list;
+        }
+
+        public static void run()
+        {
+            int[] array = newArray();
+            List<int> list = newNumbers(array);
+            foreach (int num in list)
+                Console.Write(num + ", ");
+        }
+    }
+}
